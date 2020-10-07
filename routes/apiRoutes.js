c252ac009e5eeaf2094983e7273b77b17f96f4ad
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-10-06 21:57:07 -0600

    basic functionality is present

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,23 +3,21 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
-const db = require('../db/db.json')
-
 // *** Gets All Notes
 router.get('/notes/', (req, res) => {
-    // let db = require('../db/db.json')
+    const db = require('../db/db.json')
     res.json(db)
     console.log('get all')
 });
 
 // ***Posts New Note
 router.post('/notes', (req, res) => {
-    const newNote = (req.body)
-    let db = require('../db/db.json')
-    // console.log(db)
+    const db = require('../db/db.json')
+    const newNote = { id: Date.now() + '', title: req.body.title, text: req.body.text }
+    console.log('post ', db)
     db.push(newNote)
     console.log('new note', newNote)
-    assignID();
+    // assignID();
     fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
         if (err) {
             res.send(err)
@@ -32,27 +30,24 @@ router.post('/notes', (req, res) => {
 
 // ***Deletes note with specific id
 router.delete('/notes/:id', (req, res) => {
-    // res.send('api.delete')
     let db = require('../db/db.json')
-    db.splice(req.params.id, 1);
-    assignID();
-    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.json(db)
-        }
-    })
-    // console.log(req.params)
-    console.log("Deleted note with id " + req.params.id);
-});
-
-function assignID() {
-    for (i = 0; i < db.length; i++) {
-        db[i].id = i;
+    console.log('delete', db)
+    console.log('req.params', req.params)
+    const selected = db.some(note => note.id === req.params.id)
+    if (selected) {
+        db = db.filter(note => note.id !== req.params.id)
+        console.log("Deleted note with id ", req.params.id);
+        console.log('after delete ', db)
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
+            if (err) {
+                res.send(err)
+            } else {
+                // res.json(db).reload()
+                res.json({ ok: true }).reload()
+            }
+        })
     }
-}
-
+});
 
 
 module.exports = router;
