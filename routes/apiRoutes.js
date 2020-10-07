7a4d91ea8341ccb932a801447e4facf583c719a9
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-10-06 22:11:23 -0600

    cleaned up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,17 +7,17 @@ const fs = require('fs');
 router.get('/notes/', (req, res) => {
     const db = require('../db/db.json')
     res.json(db)
-    console.log('get all')
+    // console.log('get all')
 });
 
 // ***Posts New Note
 router.post('/notes', (req, res) => {
     const db = require('../db/db.json')
-    const newNote = { id: Date.now() + '', title: req.body.title, text: req.body.text }
-    console.log('post ', db)
+    // const newNote = { id: Date.now() + '', title: req.body.title, text: req.body.text }
+    const newNote = { id: Date.now(), title: req.body.title, text: req.body.text }
+    // console.log('post ', db)
     db.push(newNote)
-    console.log('new note', newNote)
-    // assignID();
+    // console.log('new note', newNote)
     fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
         if (err) {
             res.send(err)
@@ -31,19 +31,19 @@ router.post('/notes', (req, res) => {
 // ***Deletes note with specific id
 router.delete('/notes/:id', (req, res) => {
     let db = require('../db/db.json')
-    console.log('delete', db)
-    console.log('req.params', req.params)
-    const selected = db.some(note => note.id === req.params.id)
+    // console.log('delete', db)
+    // console.log('req.params', req.params)
+    const selected = db.some(note => note.id === parseInt(req.params.id))
     if (selected) {
-        db = db.filter(note => note.id !== req.params.id)
-        console.log("Deleted note with id ", req.params.id);
-        console.log('after delete ', db)
+        db = db.filter(note => note.id !== parseInt(req.params.id))
+        // console.log("Deleted note with id ", req.params.id);
+        // console.log('after delete ', db)
         fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
             if (err) {
                 res.send(err)
             } else {
-                // res.json(db).reload()
                 res.json({ ok: true }).reload()
+                // .reload forces the html to re-render, but it also throws an error
             }
         })
     }
