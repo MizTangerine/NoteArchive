218fa22b2c4aebdef8d5d60868fc576ad204d665
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-10-07 22:14:32 -0600

    removed console logs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,10 @@ const fs = require('fs');
 
 // *** Creates HTML Routes
 router.get('/notes/', function (req, res) {
-    // console.log('html notes')
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 router.get('*', function (req, res) {
-    // console.log('html index')
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
