f720c6662b77d719634a76f666fe0866f4245ea4
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-10-07 21:46:21 -0600

    changed the way the db.json file is referenced, delete is now working properly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,52 +2,45 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
+const db = './db/db.json';
+
+function dbNotes() {
+    return JSON.parse(fs.readFileSync(db, "utf-8"));
+};
 
 // *** Gets All Notes
 router.get('/notes/', (req, res) => {
-    const db = require('../db/db.json')
-    res.json(db)
-    // console.log('get all')
+    dbNotes();
+    res.json(dbNotes());
 });
 
 // ***Posts New Note
 router.post('/notes', (req, res) => {
-    const db = require('../db/db.json')
-    // const newNote = { id: Date.now() + '', title: req.body.title, text: req.body.text }
+    const notes = dbNotes();
     const newNote = { id: Date.now(), title: req.body.title, text: req.body.text }
-    // console.log('post ', db)
-    db.push(newNote)
-    // console.log('new note', newNote)
-    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
+    notes.push(newNote)
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
         if (err) {
-            res.send(err)
+            res.send(err);
         }
         else {
-            res.json(newNote)
+            res.json(newNote);
         }
     })
 });
 
 // ***Deletes note with specific id
 router.delete('/notes/:id', (req, res) => {
-    let db = require('../db/db.json')
-    // console.log('delete', db)
-    // console.log('req.params', req.params)
-    const selected = db.some(note => note.id === parseInt(req.params.id))
-    if (selected) {
-        db = db.filter(note => note.id !== parseInt(req.params.id))
-        // console.log("Deleted note with id ", req.params.id);
-        // console.log('after delete ', db)
-        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
-            if (err) {
-                res.send(err)
-            } else {
-                res.json({ ok: true }).reload()
-                // .reload forces the html to re-render, but it also throws an error
-            }
-        })
-    }
-});
-
+    let notes = dbNotes();
+    notes = notes.filter((note => note.id !== parseInt(req.params.id)));
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.json(notes);
+        }
+    })
+})
 
 module.exports = router;
